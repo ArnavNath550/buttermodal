7fd89be832dab794a4b1722180373617eeeb3923
Arnav Nath <arnavnath550@gmail.com>
2026-03-07 01:39:21 +1300

    removed demo.html

--- dev/vite.config.ts
+++ dev/vite.config.ts
@@ -14,7 +14,6 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: path.resolve(__dirname, 'index.html'),
-        demo: path.resolve(__dirname, 'demo.html'),
       },
     },
   },
